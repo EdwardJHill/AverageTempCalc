b32bb6e95a6a1b8a4020a410e83cf5b4cc575cbc
Edward Hill <115216135+EdwardJHill@users.noreply.github.com>
2024-08-03 17:00:22 +0100

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Header.h
@@ -0,0 +1,4 @@
+#pragma once
+//we need a bunch of arrays here
+int fourth[24] = { 19,18,17,17,16,16,16,16,17,18,18,19,20,21,22,22,22,22,22,21,20,19,18};
+float calculate(int wakeUpTime, int SleepTime);
